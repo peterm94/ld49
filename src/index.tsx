66e7a5fc657b96900a70854251337c9d9315c558
Dylan Jenkins <dylanljenkins@gmail.com>
2021-10-01 22:24:28 +1000

    Updating and cleaning dependencies.

--- src/index.tsx
+++ src/index.tsx
@@ -1,44 +1,16 @@
 import './index.css';
-import * as serviceWorker from './serviceWorker';
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import {Box, Grommet} from "grommet";
-import {configure} from "mobx";
 import {LagomGameComponent} from "lagom-engine";
 import {Pong} from "./Pong";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
-configure({enforceActions: "observed"});
-
 const game = new Pong();
 
-const grommetTheme = {
-    global: {
-        font: {
-            size: '14px',
-            height: '20px',
-        }
-    }
-};
-
 const App = () => (
-    <Grommet theme={grommetTheme as any} style={{height: "100%"}}>
-        <Box direction="column" flex style={{height: "100%"}}>
-            <Box direction="row" flex fill>
-                <Box flex align="center" justify="center">
-                    <LagomGameComponent game={game}/>
-                </Box>
-                {/*<Box width="medium" background="light-2">*/}
-                {/*    <InspectorComponent game={game}/>*/}
-                {/*</Box>*/}
-            </Box>
-        </Box>
-    </Grommet>
+    <div style={{display: "flex", height: "100%", alignItems: "center", justifyContent: "center"}}>
+        <LagomGameComponent game={game}/>
+    </div>
 );
 
 ReactDOM.render(
