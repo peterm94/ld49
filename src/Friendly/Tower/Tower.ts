7d234503e2d424b15a6ca5c46608cab3c3c23232
Peter Mandile <peterm1994@hotmail.com>
2021-10-02 22:35:39 +1000

    Added turret sprite

--- src/Friendly/Tower/Tower.ts
+++ src/Friendly/Tower/Tower.ts
@@ -1,10 +1,52 @@
-import {CollisionSystem, Entity, RectCollider, RenderRect} from "lagom-engine";
+import {
+    AnimatedSpriteController,
+    AnimationEnd,
+    CollisionSystem,
+    Entity,
+    RectCollider,
+    RenderRect,
+    SpriteSheet
+} from "lagom-engine";
 import {Health} from "../../Common/Health";
 import {Ammunition} from "../../Common/Ammunition";
 import {Layers} from "../../Layers";
 import {BossRocketAttack} from "../../Enemy/Boss/BossRocketAttack";
 import {Attack} from "../../Common/Attack";
 import {HealthBar} from "../../Common/HealthBar";
+
+// 64x64
+// idle frame 0
+// 1 - 38 - shot
+import turretSpr from "../../Art/turret.png";
+// 0 - 32 idle
+// 33 - 40 drain
+// 40 empty idle
+import turretCanSpr from "../../Art/turret-canister.png";
+
+// containers 17,19 21,17, 25,15, 29,13
+
+const turretSheet = new SpriteSheet(turretSpr, 64, 64);
+const turretCan = new SpriteSheet(turretCanSpr, 7, 9);
+
+class Can extends Entity
+{
+    constructor(x: number, y: number)
+    {
+        super("can", x, y);
+    }
+
+    onAdded()
+    {
+        super.onAdded();
+
+        this.addComponent(new AnimatedSpriteController(0, [
+            {
+                id: 0,
+                textures: [turretCan.texture(0, 0)]
+            }
+        ]));
+    }
+}
 
 export class Tower extends Entity
 {
@@ -20,7 +62,23 @@ export class Tower extends Entity
         const width = 10;
         const height = 25;
 
-        this.addComponent(new RenderRect(0, 0, width, height, 0xffffff, 0xffffff));
+        this.addComponent(new AnimatedSpriteController(1, [
+            {
+                id: 0,
+                textures: turretSheet.textureSliceFromRow(0, 0, 0),
+                config: {animationEndAction: AnimationEnd.STOP}
+            },
+            {
+                id: 1,
+                textures: turretSheet.textureSliceFromRow(0, 1, 38),
+                config: {animationEndAction: AnimationEnd.STOP, animationSpeed: 60}
+            }
+        ]));
+
+        this.addChild(new Can(29, 13));
+        this.addChild(new Can(25, 15));
+        this.addChild(new Can(21, 17));
+        this.addChild(new Can(17, 19));
 
         const health = this.addComponent(new Health(100, 100));
         this.addChild(new HealthBar("tower_health", 0, 0, Layers.tower, "Tower", 0, 30));
