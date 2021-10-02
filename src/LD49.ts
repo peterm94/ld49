5c0900fd90f66da9d07db113f5e737a1686e7f6b
Chris Zappia <christopher.zappia@gmail.com>
2021-10-02 18:35:28 +1000

    Remove tile timer added

--- src/LD49.ts
+++ src/LD49.ts
@@ -1,10 +1,11 @@
-import {CollisionMatrix, DebugCollisionSystem, Diagnostics, DiscreteCollisionSystem, Game, Scene} from "lagom-engine";
+import {CollisionMatrix, DebugCollisionSystem, Diagnostics, DiscreteCollisionSystem, Game, Scene, TimerSystem} from "lagom-engine";
 import {Boss} from "./Boss";
 import {WorldGen} from "./World/WorldGen";
 import {Player, PlayerMover} from "./Player/Player";
 import {Layers} from "./Layers";
 import {GameStatusDisplay, GameStatusUpdater} from "./GameManagement/GameStatus";
 import {AmmunitionPickup} from "./Pickups/AmmunitionPickup";
+import { TileManager } from "./World/TileManager";
 
 const matrix = new CollisionMatrix();
 matrix.addCollision(Layers.player, Layers.hexagons);
@@ -32,11 +33,14 @@ class MainScene extends Scene
 
         const collSystem = this.addGlobalSystem(new DiscreteCollisionSystem(matrix));
         this.addGlobalSystem(new DebugCollisionSystem(collSystem));
+        this.addGlobalSystem(new TimerSystem());
         this.addSystem(new PlayerMover());
 
         // Game entities.
         this.addEntity(new Player(30, 30));
         this.addEntity(new AmmunitionPickup(400, 200));
+        
+        this.addEntity(new TileManager());
 
         this.addSystem(new GameStatusUpdater());
         this.addEntity(new WorldGen());
