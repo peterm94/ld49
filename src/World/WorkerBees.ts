58c3b9b76837303bdf38ef2e1bcf1ddd866eedd4
James McDowell <wejrox@users.noreply.github.com>
2021-10-04 13:17:39 +1100

    Revert "MOAR BG BEES, RANDOM ANGLE TOO"
    This reverts commit 4d559ec4553ac15cfa6635826786b64cc29f34b2.

--- src/World/WorkerBees.ts
+++ src/World/WorkerBees.ts
@@ -1,20 +1,13 @@
 import killerBeeSpr from "../Art/killer-bee.png";
-import {AnimatedSprite, Component, Entity, MathUtil, SpriteSheet, System, Timer, Util, Vector} from "lagom-engine";
+import {AnimatedSprite, Component, Entity, MathUtil, SpriteSheet, System, Timer, Util} from "lagom-engine";
 
 const killerBee = new SpriteSheet(killerBeeSpr, 5, 5);
 
 export class BackgroundBees extends Entity
 {
-    constructor()
-    {
-        super("bgbee", 0, 0, -99999999);
-    }
-
     spawnBee()
     {
-        const minSpawnTimeMs = 0;
-        const maxSpawnTimeMs = 1_000;
-        this.addComponent(new Timer(MathUtil.randomRange(minSpawnTimeMs, maxSpawnTimeMs), null, false)).onTrigger
+        this.addComponent(new Timer(MathUtil.randomRange(5000, 4000), null, false)).onTrigger
             .register((caller, data) => {
                 const bee = this.addChild(new Entity("bgbeeee", Util.choose(-20, 450), MathUtil.randomRange(88, 240)));
                 bee.addComponent(new AnimatedSprite(killerBee.textureSliceFromRow(0, 0, 1),
@@ -22,11 +15,14 @@ export class BackgroundBees extends Entity
                         yAnchor: 0.5, xAnchor: 0.5, animationSpeed: 50,
                         rotation: bee.transform.position.x > 0 ? MathUtil.degToRad(270) : MathUtil.degToRad(90)
                     }));
-                const beeTarget = new Vector(bee.transform.position.x > 0 ? -1 : 1,
-                    MathUtil.randomRange(-1, 2));
-                bee.addComponent(new BgBee(beeTarget, MathUtil.randomRange(50, 200)));
+                bee.addComponent(new BgBee(bee.transform.position.x > 0 ? -1 : 1, MathUtil.randomRange(50, 200)));
                 this.spawnBee();
             });
+    }
+
+    constructor()
+    {
+        super("bgbee", 0, 0, -99999999);
     }
 
     onAdded()
@@ -41,7 +37,7 @@ export class BackgroundBees extends Entity
 
 class BgBee extends Component
 {
-    constructor(readonly dir: Vector, readonly speed: number)
+    constructor(readonly dir: number, readonly speed: number)
     {
         super();
     }
@@ -54,8 +50,7 @@ class BeeMover extends System
     update(delta: number): void
     {
         this.runOnEntities((entity: Entity, bee: BgBee) => {
-            entity.transform.position.x += bee.dir.x * bee.speed * delta / 1000;
-            entity.transform.position.y += bee.dir.y * bee.speed * delta / 1000;
+            entity.transform.position.x += bee.dir * bee.speed * delta / 1000;
 
             if (entity.transform.position.x < -100 || entity.transform.position.x > 1000)
             {
